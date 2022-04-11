28c3fc6f540cc1079ddaa6e6d437e6576318da2c
Mariana Tapia <mglopez2113@gmail.com>
2022-04-10 22:24:51 -0700

    updating js

--- script.js
+++ script.js
@@ -2,29 +2,44 @@ var apiKey = "c95583aac1e8c2808350a9dfe6ba7c63"
 
 function oneCallApi() {
 
-    var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=32.715736&lon=-117.161087&exclude=hourly,minutely,alerts&appid=" + apiKey;
-  
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-          console.log(data);
-        });
-    }
-    //oneCallApi();
+  var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=32.715736&lon=-117.161087&exclude=hourly,minutely,alerts&appid=" + apiKey;
 
-    function geolocationApi() {
-        var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=San Diego&limit=5&appid=" + apiKey;
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      //appendData(data);
+    })
+  }
+    //function displayWeather(data)  
+      //for (var i = 0; i < data.length; i++) {
+        //ul.innerHTML = "curent" + data[i].humidity + '' + data[i].temp;
+        //weatherContainer.appendChild("sandiego");
+      //}
+      
+
   
-        fetch(requestUrl)
-          .then(function (response) {
-            return response.json();
-          })
-          .then(function (data) {
-              console.log(data);
-            });
-        
-        }
-geolocationApi();
-    
+    //.then(function (data) {
+      //console.log(data);
+  
+        //var tempWeather = data.current
+      //var weatherContainer = document.getElementById("weatherelements");
+      //for (var i = 0; i < data.length; i++) {
+        //ul.innerHTML = "curent" + data[i].humidity + '' + data[i].temp;
+        //weatherContainer.appendChild("sandiego");
+      //}
+   
+    //}}
+  
+
+//function geolocationApi() {
+  //var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=San Diego&limit=5&appid=" + apiKey;
+
+  //fetch(requestUrl)
+    //.then(function (response) {
+      //return response.json();
+    //})
+    //.then(function (data) {
+      //console.log(data);
